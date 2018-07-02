9a8d1339a2429c66aa3d91d5fdeb7dfad04bef94
Matheus de Sousa Barros <bmatheus91@gmail.com>
2018-07-02 15:22:30 -0300

    Refatoracao

---
+++ README.md
@@ -0,0 +1,40 @@
+# Eventex
+
+Sistema de eventos do curso WTTD
+
+## Como Desenvolver?
+
+1. Clone o repositório
+2. Crie um virtualenv com python 3.5
+3. Ative o virtualenv
+4. Instale as dependencias
+5. Configure a instância com o .env
+6. Execute os testes
+
+```console
+git clone https://github.com/bmatheus91/eventex.git wttd
+cd wttd
+python -m venv .wttd
+source ./wttd/bin/activate
+pip install -r requirements.txt
+cp contrib/env-sample .env
+python manage.py test
+```
+
+## Como fazer o Deploy?
+
+1. Crie uma instância pro heroku
+2. Envie as configurações pro heroku
+3. Defina a SECRET_KEY
+4. Defina DEGUB = False
+5. Configure o serviço de Email
+6. Envie o código pro heroku
+
+```console
+heroku create instancia
+heroku config:push
+heroku config:set SECRET_KEY=python contrib/secret_gen.py
+heroku config:set DEBUG=False
+#Configuração do email
+git push heroku master --force
+```
---
+++ eventex/subscriptions/tests/test_mail_subscribe.py
@@ -0,0 +1,48 @@
+from django.core import mail
+from django.test import TestCase
+
+
+class SubscribePostValid(TestCase):
+
+    def setUp(self):
+        data = dict(
+            name='Matheus de Sousa Barros',
+            cpf='02824886340',
+            email='bmatheus91@gmail.com',
+            phone='98982858442'
+        )
+
+        self.client.post('/inscricao/', data)
+        self.email = mail.outbox[0]
+
+    def test_send_subscribe_email(self):
+        """Before redirect valid POST, send email to subscriber"""
+        self.assertEqual(1, len(mail.outbox))
+
+    def test_subscription_email_subject(self):
+        """Email subject must be Confirmação de Inscrição"""
+        expect = 'Confirmação de Inscrição'
+        self.assertEqual(expect, self.email.subject)
+
+    def test_subscription_email_from(self):
+        """Email from must be contato@eventex.com.br"""
+        expect = 'contato@eventex.com.br'
+        self.assertEqual(expect, self.email.from_email)
+
+    def test_subscription_email_to(self):
+        """Email to must be contato@eventex.com.br and subscribers email"""
+        expect = ['contato@eventex.com.br', 'bmatheus91@gmail.com']
+        self.assertEqual(expect, self.email.to)
+
+    def test_subscription_email_body(self):
+        """Email body must contain subscribers information"""
+        contents = [
+            'Matheus de Sousa Barros',
+            '02824886340',
+            'bmatheus91@gmail.com',
+            '98982858442',
+        ]
+
+        for content in contents:
+            with self.subTest():
+                self.assertIn(content, self.email.body)
