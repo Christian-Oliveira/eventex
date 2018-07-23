3f50abcc59e14cf904b48800331e3dcfb4a46334
Matheus de Sousa Barros <bmatheus91@gmail.com>
2018-07-23 10:46:00 -0300

    Palestrantes

--- eventex/core/tests/test_view_home.py
+++ eventex/core/tests/test_view_home.py
@@ -21,3 +21,20 @@ class HomeTest(TestCase):
         """HTML must contain href='/inscricao/' link"""
         expected = 'href="{}"'.format(r('subscriptions:new'))
         self.assertContains(self.response, expected)
+
+    def test_speakers(self):
+        """Must show key note speakers"""
+        content = [
+            'Grace Hopper',
+            'http://hbn.link/hopper-pic',
+            'Alan Turing',
+            'http://hbn.link/turing-pic'
+        ]
+
+        for expected in content:
+            with self.subTest():
+                self.assertContains(self.response, expected)
+
+    def test_speakers_link(self):
+        expected = 'href="{}#speakers"'.format(r('home'))
+        self.assertContains(self.response, expected)
